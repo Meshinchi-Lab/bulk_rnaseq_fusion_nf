b76622275aafcc4fc8a327a11900ea882a0c7a8f
Jenny Smith <jennyl.smith12@gmail.com>
2020-07-13 18:43:39 -0700

    03p1 sample

--- STAR_Fusion_run.sh
+++ STAR_Fusion_run.sh
@@ -10,10 +10,10 @@ ml nextflow/20.04.0-edge
 
 #Execute the next flow workflow  $BASE_BUCKET/work
 nextflow run -c ~/nextflow.config STAR_Fusion.nf \
-    --sample_sheet sample_sheets/DS.AML_sample_sheet.txt \
+    --sample_sheet sample_sheets/NUP98-NAPEPLD_sample_sheet.txt \
     --genome_lib $BASE_BUCKET/starfusion/GRCh37_gencode_v19_CTAT_lib_Oct012019/ctat_genome_lib_build_dir \
-    --output_folder  $BASE_BUCKET/starfusion/DS_AML \
-    -with-report STAR-Fusion_DS.AML_report.html \
+    --output_folder  $BASE_BUCKET/starfusion/ \
+    -with-report STAR-Fusion_NUP98-NAPEPLD_report.html \
     -work-dir $BASE_BUCKET/work \
     -cache  TRUE \
     -resume
