e79d20dc1fd0ded8c9e362596c036b104566f900
jennylsmith <jennyl.smith12@gmail.com>
2020-09-28 15:30:48 -0700

    added st.jude reports

--- STAR_Fusion_run.sh
+++ STAR_Fusion_run.sh
@@ -15,7 +15,7 @@ ml nextflow/20.04.0-edge
 #Execute the next flow workflow  $BASE_BUCKET/work
 nextflow run -c ~/nextflow.config STAR_Fusion.nf \
     --sample_sheet sample_sheets/st.jude_sample_sheet.txt \
-    --genome_lib $BASE_BUCKET/Reference_Data/GRCh37_gencode_v19_CTAT_lib_Oct012019/ \
+    --genome_lib $BASE_BUCKET/Reference_Data/GRCh37_gencode_v19_CTAT_lib_Oct012019/ctat_genome_lib_build_dir/ \
     --output_folder  $BASE_BUCKET/starfusion/ \
     -with-report STAR-Fusion_St.Jude_report.html \
     -work-dir $BASE_BUCKET/work \
